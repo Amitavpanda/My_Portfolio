51b374eb51ce17b2889f19468dda0f2155aaf856
Amitav Panda <pandaamitav01@gmail.com>
2024-02-21 10:29:26 -0800

    made changes to check the bug fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,18 +80,18 @@ const Works = () => {
               </motion.p>
             </div>
           ))}
-
-
-          <div className="mt-20 flex flex-wrap gap-7">
-            {projects.map((project, index) => (
-              <>
-                <ProjectCard key={`work-${index}`} {...project} />
-              </>
-            ))}
-          </div>
         </>
 
       ))}
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <>
+            <ProjectCard key={`work-${index}`} {...project} />
+          </>
+        ))}
+      </div>
+
 
     </>
   );
